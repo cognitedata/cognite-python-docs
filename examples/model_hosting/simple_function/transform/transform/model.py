79abbcfea67b2babb840d236e794216974cb0c08
NilsBarlaug-cognite <40356634+NilsBarlaug-cognite@users.noreply.github.com>
2019-04-30 10:25:30 +0200

    Update Model Hosting examples to 0.1.2 (#18)

--- examples/model_hosting/simple_function/transform/transform/model.py
+++ examples/model_hosting/simple_function/transform/transform/model.py
@@ -12,5 +12,5 @@ class Model:
     def predict(self, instance):
         data_fetcher = DataFetcher(instance)
         df = data_fetcher.time_series.fetch_dataframe(["x1", "x2"])
-        df["y"] = (df["x1"] + df["x2"]) * math.pi
+        df["y"] = (df["x1"] + df["x2"]) / math.pi
         return to_output(df[["y", "timestamp"]])
