81a19125db12f66dd42c2067a331f84c4b675e23
Haukb <36861726+haukb@users.noreply.github.com>
2019-08-12 10:00:18 +0200

    Added schedule inspection notebook (#26)

---
+++ examples/model_hosting/schedule_troubleshooting/cpr/cpr/model.py
@@ -0,0 +1,15 @@
+from cognite.model_hosting.data_fetcher import DataFetcher
+from cognite.model_hosting.schedules import to_output
+
+
+class Model:
+    @staticmethod
+    def load(open_artifact):
+        return Model()
+
+    def predict(self, instance):
+        data_fetcher = DataFetcher(instance)
+        df = data_fetcher.time_series.fetch_dataframe(["x1", "x2"])
+        df["y"] = df["x2"] / df["x1"]
+
+        return to_output(df.size)
