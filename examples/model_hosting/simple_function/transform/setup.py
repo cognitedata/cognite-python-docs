c0d28426993e2b223c112c9dc2d2337733373a3c
Henrik Eiding <henrik.eiding@cognite.com>
2019-11-07 16:07:40 +0100

    Update simple_function

--- examples/model_hosting/simple_function/transform/setup.py
+++ examples/model_hosting/simple_function/transform/setup.py
@@ -1,5 +1,5 @@
 from setuptools import find_packages, setup
 
-REQUIRED_PACKAGES = ["cognite-model-hosting==0.1.*"]
+REQUIRED_PACKAGES = ["cognite-model-hosting==0.2.0a1"]
 
 setup(name="transform", version="0.1", install_requires=REQUIRED_PACKAGES, packages=find_packages())
