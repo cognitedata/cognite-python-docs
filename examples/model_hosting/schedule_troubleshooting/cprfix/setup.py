b03f2eb56b28c039dac33995e490437db2a1d58b
Henrik Eiding <henrik.eiding@cognite.com>
2019-11-12 10:05:30 +0100

    Remove alpha-version from cognite-model-hosting package.

--- examples/model_hosting/schedule_troubleshooting/cprfix/setup.py
+++ examples/model_hosting/schedule_troubleshooting/cprfix/setup.py
@@ -1,5 +1,5 @@
 from setuptools import find_packages, setup
 
-REQUIRED_PACKAGES = ["cognite-model-hosting==0.2.0a1"]
+REQUIRED_PACKAGES = ["cognite-model-hosting==0.2.0"]
 
 setup(name="cprfix", version="0.1", install_requires=REQUIRED_PACKAGES, packages=find_packages())
